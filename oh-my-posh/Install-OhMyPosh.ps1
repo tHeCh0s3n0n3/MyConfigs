7de13bae295072ef902f46e6748ba6d16e689a8f
Tarek Fadel <dev@tarekfadel.com>
2022-06-18 19:55:21 +0400

    Make the Powershell OhMyPosh install script also set up $PROFILE if it doesn't exist

--- oh-my-posh/Install-OhMyPosh.ps1
+++ oh-my-posh/Install-OhMyPosh.ps1
@@ -1,13 +1,22 @@
 Write-Output "Installing Oh My Posh"
-winget install JanDeDobbeleer.OhMyPosh
+#winget install JanDeDobbeleer.OhMyPosh
 
 Write-Output "Installing fonts"
-Start-Process powershell -Verb RunAs "oh-my-posh font install"
+#Start-Process powershell -Verb RunAs "oh-my-posh font install"
 
 Write-Output "Oh My Posh and fonts are installed"
 Write-Output "#########"
 Write-Output "# Notes #"
 Write-Output "#########"
 Write-Output "- Remember to use the new fonts"
-Write-Output "- Update your prompt to use oh-my-posh"
 Write-Output "- Restart your terminal to load the new path"
+Write-Output "- Add 'oh-my-posh init pwsh | Invoke-Expression' to $PROFILE"
+if (-Not (Test-Path -Path $PROFILE)) {
+    Write-Host -NoNewline "  Adding Oh My Posh to profile automatically..."
+    [void](New-Item -Path $PROFILE -Type File -Force -Value @'
+    $host.PrivateData.ErrorForegroundColor = "White"
+    $host.PrivateData.ErrorBackgroundColor = "Red"
+    oh-my-posh init pwsh | Invoke-Expression
+'@)
+    Write-Host " Done"
+}
