8fdc7dbda7a9ad5c05cfa6ddf3abfb469bdc6126
Tarek Fadel <dev@tarekfadel.com>
2022-06-18 20:02:28 +0400

    Add output to indicate copying the oh-my-posh config

--- Copy-Configs.ps1
+++ Copy-Configs.ps1
@@ -1,4 +1,5 @@
 Write-Output "Copying .gitconfig file to home directory"
 Copy-Item .\.gitconfig $HOME\.gitconfig
 
+Write-Output "Copying oh-my-posh custom config"
 Copy-Item .\oh-my-posh\tarekf.omp.yaml $HOME\AppData\Local\Programs\oh-my-posh\themes\tarekf.omp.yaml
